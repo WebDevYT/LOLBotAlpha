494cc55dcd6d09d1d450be64f48e0fd71e43e90c
web <78394517+WebDevYT@users.noreply.github.com>
2022-05-16 15:42:15 +0000

    X

---
+++ events/ready.js
@@ -0,0 +1,10 @@
+// Ready Event
+// Bot will start receiving info from Discord ONLY after this
+
+module.exports = {
+	name: 'ready',
+	once: true,
+	execute(client) {
+		console.log(`Ready! Logged in as ${client.user.tag}`);
+	},
+};
--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 // To read cmd files
 const fs = require('node:fs');
+const path = require('node:path');
 
 // Importing & requiring discord.js modules / classes 
 const { Client, Collection, Intents } = require('discord.js')
@@ -14,23 +15,18 @@ const token = process.env['token']
 // Creating new Discord client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.commands = new Collection();
-
 // To dynamically retrieving cmd files
+client.commands = new Collection();
+const cmdsPath = path.join(__dirname, 'commands');
 const cmdFiles = fs.readdirSync('./cmds').filter(file => file.endsWith('.js'));
 
 for (const file of cmdFiles) {
+    const filePath = path.join(cmdsPath, file);
 	const cmd = require(`./cmds/${file}`);
 	// Set a new item in the Collection
 	// With the key as the command name and the value as the exported module
 	client.commands.set(cmd.data.name, cmd);
 }
-
-// Ready Event
-// Bot will start receiving info from Discord ONLY after this
-client.once('ready', c => {
-    console.log(`Ready! Logged in as ${c.user.tag}!`);
-});
 
 // Interaction listener
 // To reply to cmds
