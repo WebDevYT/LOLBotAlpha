185c7f07b498a176a2aa0f8b648bfb056081f0d3
web <78394517+WebDevYT@users.noreply.github.com>
2022-05-17 17:21:20 +0000

    No real changes made. discord.js Update

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,19 +1,13 @@
 'use strict';
 
-// ping command
-
-// Importing & requiring discord.js modules / classes 
-const { Client, Intents } = require('discord.js')
-
-// Creating new Discord client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+// ping cmd
 
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Replies with Pong!'),
+        .setDescription(`Replies with Bot's API Latency`),
     async execute(interaction) {
         const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
         interaction.editReply(`Pong! \`${sent.createdTimestamp - interaction.createdTimestamp}ms\``);
